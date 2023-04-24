7e25b090c2eea7a9e23de6ac25d271e2dbe58fe9
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-24 18:52:26 +0000

    Add calculator rows

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Calculator = () => (
-  <div>
+  <div className="calculator">
     <div className="result">
       <span>0</span>
     </div>
@@ -10,7 +10,30 @@ const Calculator = () => (
       <Key name="AC" />
       <Key name="+/-" />
       <Key name="%" />
-      <Key name="÷" />
+      <Key name="÷" className="yellow" />
+    </div>
+    <div className="row">
+      <Key name="7" />
+      <Key name="8" />
+      <Key name="9" />
+      <Key name="X" className="yellow" />
+    </div>
+    <div className="row">
+      <Key name="4" />
+      <Key name="5" />
+      <Key name="6" />
+      <Key name="-" className="yellow" />
+    </div>
+    <div className="row">
+      <Key name="1" />
+      <Key name="2" />
+      <Key name="3" />
+      <Key name="+" className="yellow" />
+    </div>
+    <div className="row">
+      <Key name="0" className="zero" />
+      <Key name="." />
+      <Key name="=" className="yellow" />
     </div>
   </div>
 );
