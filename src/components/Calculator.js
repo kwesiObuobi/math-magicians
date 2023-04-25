3b5f4cf7c94a0230e32e5b06f18bf6ea315f9d2a
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-25 12:57:24 +0000

    Refactor calculator keys into an array of arrays

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="result">
-      <span>0</span>
+const Calculator = () => {
+  const [object, setObject] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (button) => {
+    setObject(calculate(object, button));
+  };
+
+  const calculatorBoard = [
+    ['AC', '+/-', '%', '÷'],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
+
+  return (
+    <div className="calculator">
+      <div className="result">
+        <span>0</span>
+      </div>
+      <div className="row">
+        <Key name="AC" />
+        <Key name="+/-" />
+        <Key name="%" />
+        <Key name="÷" tag="yellow" />
+      </div>
+      <div className="row">
+        <Key name="7" />
+        <Key name="8" />
+        <Key name="9" />
+        <Key name="X" tag="yellow" />
+      </div>
+      <div className="row">
+        <Key name="4" />
+        <Key name="5" />
+        <Key name="6" />
+        <Key name="-" tag="yellow" />
+      </div>
+      <div className="row">
+        <Key name="1" />
+        <Key name="2" />
+        <Key name="3" />
+        <Key name="+" tag="yellow" />
+      </div>
+      <div className="row">
+        <Key name="0" tag="zero" />
+        <Key name="." />
+        <Key name="=" tag="yellow" />
+      </div>
     </div>
-    <div className="row">
-      <Key name="AC" />
-      <Key name="+/-" />
-      <Key name="%" />
-      <Key name="÷" tag="yellow" />
-    </div>
-    <div className="row">
-      <Key name="7" />
-      <Key name="8" />
-      <Key name="9" />
-      <Key name="X" tag="yellow" />
-    </div>
-    <div className="row">
-      <Key name="4" />
-      <Key name="5" />
-      <Key name="6" />
-      <Key name="-" tag="yellow" />
-    </div>
-    <div className="row">
-      <Key name="1" />
-      <Key name="2" />
-      <Key name="3" />
-      <Key name="+" tag="yellow" />
-    </div>
-    <div className="row">
-      <Key name="0" tag="zero" />
-      <Key name="." />
-      <Key name="=" tag="yellow" />
-    </div>
-  </div>
-);
+  );
+};
 
 const Key = ({ name, tag }) => <button type="button" className={`key ${tag}`}>{ name }</button>;
 
