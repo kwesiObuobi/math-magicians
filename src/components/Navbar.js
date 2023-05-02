cbf1f3fe5d07f2560c64d249fd6625627ae06de9
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-02 14:18:42 +0000

    Remove margin bottom from navbar on large screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/navbar.css';
 
 const Navbar = () => (
-  <nav>
-    <h1 className="logo">Math Magicians</h1>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/calculator">Calculator</NavLink>
-    <NavLink to="/quote">Quote</NavLink>
-  </nav>
+  <div className="nav-container">
+    <nav className="navbar">
+      <h1 className="logo">Math Magicians</h1>
+      <ul className="nav-links">
+        <li><NavLink to="/" className="nav-link">Home</NavLink></li>
+        <li><NavLink to="/calculator" className="nav-link">Calculator</NavLink></li>
+        <li><NavLink to="/quote" className="nav-link">Quote</NavLink></li>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Navbar;
