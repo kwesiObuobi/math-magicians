a75f49205d964417eb3556bdee6cdf435e3f6414
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-02 11:56:11 +0000

    Add Navbar component, import it into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 import Quote from './components/quote';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Calculator />
       <Quote />
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div>
+    <p>This is the navbar</p>
+    <p>It has 2 p tags</p>
+  </div>
+);
+
+export default Navbar;
