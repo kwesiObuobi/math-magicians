65779e1f0cef46fcf49f5189f178c319ea1f7490
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-02 13:56:49 +0000

    Add css variables for responsive container values

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-// import Calculator from './components/Calculator';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Quote from './components/quote';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Outlet />
-      {/* <Calculator />
-      <Quote /> */}
     </div>
   );
 }
